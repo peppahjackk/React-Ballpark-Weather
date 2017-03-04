cbf3805c017976b83ab50d79004382b3a644aa05
theodore.moke <theodore.moke@gmail.com>
2017-03-04 12:31:33 -0500

    added semantic UI CSS link, cleaned up index.js

--- app/index.js
+++ app/index.js
@@ -3,14 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './config/routes.js'
 
-/* class HelloWorld extends React.Component {
-	render() {
-		return (
-			<div>ES6 helllerr</div>
-		)
-	}
-} */
-
 var _APP_INFO = {
   name: 'Baseball-Weather',
   branch: 'master',
