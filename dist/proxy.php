51c4a48c8850d734ab6232630f907e0b99366c6e
theodore.moke <theodore.moke@gmail.com>
2017-03-27 12:20:49 -0400

    Added readme, testing proxy

--- dist/proxy.php
+++ dist/proxy.php
@@ -5,15 +5,16 @@ ini_set("display_errors", "1"); // shows all errors
 ini_set("log_errors", 1);
 ini_set("error_log", "/tmp/php-error.log");
 
+$locations = $_POST("locations");
+
 $curl = curl_init();
 
 curl_setopt($curl, CURLOPT_URL, "https://api.darksky.net/forecast/04497081d270b956aac2ccc286d0b245/40.829327,-73.927735");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($curl);
-echo $result;
+//echo $result;
 curl_close($curl)
-
-/* $result = 6-3; */
-
+  
+echo $locations;
 
 ?>
