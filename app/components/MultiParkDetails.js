6a0f6843d7aef8a8418a7c5ca2a4472aa091016c
theodore.moke <theodore.moke@gmail.com>
2017-04-18 17:45:54 -0400

    Styled weather details and list

--- app/components/MultiParkDetails.js
+++ app/components/MultiParkDetails.js
@@ -8,7 +8,7 @@ export default class MultiParkDetails extends React.Component {
   }
   
   render() {
-    let domeParks = [], outdoorParks = [], lowChanceParks = [];
+    let domeParks = [], outdoorParks = [], lowChanceParks = [], emptyPark = [];
     for (let i = 0; i < this.props.parks.length; i++) {
       if (['ARI','HOU','MIA','MIL','SEA','TB','TOR'].indexOf(this.props.parks[i].home_name_abbrev) > -1) {
         domeParks.push(this.props.parks[i]);
@@ -18,17 +18,21 @@ export default class MultiParkDetails extends React.Component {
         outdoorParks.push(this.props.parks[i]);
       }
     }
+    let lowChanceNum = lowChanceParks.length + domeParks.length;
+    if (lowChanceNum % 2) {
+      emptyPark.push('-');
+    }
     return (
       <Grid.Column style={styles.details}>
         <Header as='h3' style={styles.infoHeader}>{this.props.dateInfo['Day'+this.props.day]}</Header>
         <Header as='h4' style={styles.infoSubHeader}>{this.props.dateInfo['Day'+this.props.day+'Date']}</Header>
           <div style={styles.detailsContainer}>
             <ul style={Object.assign({}, styles.list, styles.highChance)}>
-            {outdoorParks.map((park) => <li key={park.home_name_abbrev+this.props.day}>{park.away_name_abbrev} vs {park.home_name_abbrev} {Math.round(this.props.data[park.home_name_abbrev].daily.data[this.props.day].precipProbability * 100)}% {this.props.data[park.home_name_abbrev].daily.data[this.props.day].precipType || 'rain'}</li>)} </ul>
+            {outdoorParks.map((park) => <li key={park.home_name_abbrev+this.props.day} style={styles.listItem}>{park.away_name_abbrev} vs {park.home_name_abbrev} {Math.round(this.props.data[park.home_name_abbrev].daily.data[this.props.day].precipProbability * 100)}% {this.props.data[park.home_name_abbrev].daily.data[this.props.day].precipType || 'rain'}</li>)} </ul>
             <ul style={Object.assign({}, styles.list, styles.lowChance)}>
-              {lowChanceParks.map((park) => <li key={park.home_name_abbrev+this.props.day}>{park.away_name_abbrev} vs {park.home_name_abbrev} {Math.round(this.props.data[park.home_name_abbrev].daily.data[this.props.day].precipProbability * 100)}%</li>)}
-            {domeParks.map((park) => <li key={park.home_name_abbrev+this.props.day}>{park.away_name_abbrev} vs {park.home_name_abbrev} DOME</li>)}</ul>
-        <div style={styles.ulGrass}></div></div>
+              {lowChanceParks.map((park) => <li key={park.home_name_abbrev+this.props.day} style={styles.listItem}>{park.away_name_abbrev} vs {park.home_name_abbrev} {Math.round(this.props.data[park.home_name_abbrev].daily.data[this.props.day].precipProbability * 100)}%</li>)}
+            {domeParks.map((park) => <li key={park.home_name_abbrev+this.props.day} style={styles.listItem}>{park.away_name_abbrev} vs {park.home_name_abbrev} DOME</li>)}
+                           {emptyPark.map((park) => <li key={'emptyPark'+this.props.day} style={styles.listItem}>-</li>)}</ul></div>
       </Grid.Column>
     )
   }
