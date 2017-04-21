fc2dedc1bb16595df7204d1d9b28eb3a6feb608a
theodore.moke <theodore.moke@gmail.com>
2017-04-21 12:10:15 -0400

    Installed table, still a list for low chance

--- app/components/MultiParkDetails.js
+++ app/components/MultiParkDetails.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Grid, Header } from 'semantic-ui-react'
+import { Grid, Header, Table } from 'semantic-ui-react'
 import styles from '../styles'
+import PrecipPercent from './PrecipPercent'
+import PrecipType from './PrecipType'
+import LowChancePrecip from './LowChancePrecip'
 
 export default class MultiParkDetails extends React.Component {
   constructor(props) {
@@ -8,14 +11,14 @@ export default class MultiParkDetails extends React.Component {
   }
   
   render() {
-    let domeParks = [], outdoorParks = [], lowChanceParks = [], emptyPark = [];
+    let domeParks = [], highChanceParks = [], lowChanceParks = [], emptyPark = [];
     for (let i = 0; i < this.props.parks.length; i++) {
       if (['ARI','HOU','MIA','MIL','SEA','TB','TOR'].indexOf(this.props.parks[i].home_name_abbrev) > -1) {
         domeParks.push(this.props.parks[i]);
       } else if (this.props.data[this.props.parks[i].home_name_abbrev].daily.data[this.props.day].precipProbability < .4) {
         lowChanceParks.push(this.props.parks[i]);
       } else {
-        outdoorParks.push(this.props.parks[i]);
+        highChanceParks.push(this.props.parks[i]);
       }
     }
     let lowChanceNum = lowChanceParks.length + domeParks.length;
@@ -29,12 +32,31 @@ export default class MultiParkDetails extends React.Component {
           <Header as='h4' style={styles.infoSubHeader}>{this.props.dateInfo['Day'+this.props.day+'Date']}</Header>
         </div>
           <div style={styles.detailsContainer}>
-            <ul style={Object.assign({}, styles.list, styles.highChance)}>
-            {outdoorParks.map((park) => <li key={park.home_name_abbrev+this.props.day} style={Object.assign({}, styles.listItem, styles.highChanceItem)}>{park.away_name_abbrev} vs {park.home_name_abbrev} {Math.round(this.props.data[park.home_name_abbrev].daily.data[this.props.day].precipProbability * 100)}% {this.props.data[park.home_name_abbrev].daily.data[this.props.day].precipType || 'rain'}</li>)} </ul>
+            <Table celled style={styles.precipTable}>
+              <Table.Header style={styles.precipTHead}>
+                <Table.Row>
+                  <Table.HeaderCell>Matchup</Table.HeaderCell>
+                  <Table.HeaderCell>Precip %</Table.HeaderCell>
+                  <Table.HeaderCell>Type</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+      
+            <Table.Body>
+              {highChanceParks.map((park) =>
+                <Table.Row key={park.home_name_abbrev+this.props.day}>
+                  <Table.Cell style={styles.precipCell}>{park.home_name_abbrev} vs {park.away_name_abbrev}</Table.Cell>
+                  <PrecipPercent data={this.props.data[park.home_name_abbrev]} day={this.props.day}/>
+                  <PrecipType data={this.props.data[park.home_name_abbrev]} day={this.props.day} />
+                </Table.Row>
+               )}
+            </Table.Body>
+          </Table>
+            <Header as='h4' style={styles.infoSubHeader}>Low/No Chance Parks</Header>
             <ul style={Object.assign({}, styles.list, styles.lowChance)}>
               {lowChanceParks.map((park) => <li key={park.home_name_abbrev+this.props.day} style={styles.listItem}>{park.away_name_abbrev} vs {park.home_name_abbrev} {Math.round(this.props.data[park.home_name_abbrev].daily.data[this.props.day].precipProbability * 100)}%</li>)}
             {domeParks.map((park) => <li key={park.home_name_abbrev+this.props.day} style={styles.listItem}>{park.away_name_abbrev} vs {park.home_name_abbrev} DOME</li>)}
-                           {emptyPark.map((park) => <li key={'emptyPark'+this.props.day} style={styles.listItem}>-</li>)}</ul></div>
+                           {emptyPark.map((park) => <li key={'emptyPark'+this.props.day} style={styles.listItem}>-</li>)}</ul>
+        </div>
       </Grid.Column>
     )
   }
---
+++ app/components/PrecipType.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Table } from 'semantic-ui-react'
+import styles from '../styles'
+
+export default class PrecipPercent extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    return(
+      <Table.Cell style={styles.precipCell}>
+        {this.props.data.daily.data[this.props.day].precipType || 'rain'}
+      </Table.Cell>
+    )
+  }
+}
