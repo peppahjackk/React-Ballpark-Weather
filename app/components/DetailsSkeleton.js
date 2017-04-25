7216b957aae4ea453463ae32f0985aba5516af53
theodore.moke <theodore.moke@gmail.com>
2017-04-25 18:28:27 -0400

    Changed font, matched skeleton, added loading dimmer

--- app/components/DetailsSkeleton.js
+++ app/components/DetailsSkeleton.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Segment, Header } from 'semantic-ui-react'
+import { Grid, Segment, Header, Table } from 'semantic-ui-react'
 import styles from '../styles'
 
 export default class DetailsSkeleton extends React.Component {
@@ -9,13 +9,34 @@ export default class DetailsSkeleton extends React.Component {
   
   render() {
     return (
-      <Grid.Column>
+      <Grid.Column tablet={16} mobile={16} computer={5}>
         <div style={styles.detailsContainer}>
           <Header as='h3' style={styles.infoHeader}>Weekday</Header>
-          <Header as='h4' style={styles.infoSubHeader}>Date</Header>
+          <Header as='h4' style={Object.assign({},styles.infoHeader,styles.noMarginTop)}>Date</Header>
         </div>
         <div style={Object.assign({}, styles.detailsContainer, styles.skeleton)}>
-          
+          <Table celled compact unstackable style={styles.precipTable}>
+            <Table.Header style={styles.precipTHead}>
+              <Table.Row>
+                <Table.HeaderCell>Matchup</Table.HeaderCell>
+                <Table.HeaderCell>Precip %</Table.HeaderCell>
+                <Table.HeaderCell>Type</Table.HeaderCell>
+              </Table.Row>
+            
+              <Table.Row>
+                <Table.Cell>-</Table.Cell>
+                <Table.Cell>-</Table.Cell>
+                <Table.Cell>-</Table.Cell>
+              </Table.Row>
+            
+              <Table.Row>
+                <Table.Cell>-</Table.Cell>
+                <Table.Cell>-</Table.Cell>
+                <Table.Cell>-</Table.Cell>
+              </Table.Row>
+            </Table.Header>
+          </Table>
+            <Header as='h4' style={Object.assign({},styles.infoHeader,styles.noMarginTop)}>Low or No Chance Parks</Header>
         </div>
       </Grid.Column>
     )
