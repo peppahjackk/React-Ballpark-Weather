cfe7c1929d7b1ad4cfdaea96528027fe4e84d738
theodore.moke <theodore.moke@gmail.com>
2017-05-01 15:24:18 -0400

    Added app footer

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { Grid, Button, Header, Container } from 'semantic-ui-react'
 import PageHeader from './PageHeader'
 import FiveDayLeague from './FiveDayLeague'
+import Footer from './Footer'
 import styles from '../styles'
 
 
 export default class Home extends React.Component {
   render() {
     return (
-      <Container style={styles.container}>
+      <Container fluid style={styles.container}>
         <Grid columns='16' centered style={styles.fullGrid}>
           <Grid.Row columns='16'>    
             <PageHeader cols='16'></PageHeader>
           </Grid.Row>
           <FiveDayLeague cols='16' header='Ballpark Weather'></FiveDayLeague>
         </Grid>
+      <Footer />
       </Container>
     )
   }
