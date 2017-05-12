76f18c83f5197eddadbf366ad535ad39198be1d5
theodore.moke <theodore.moke@gmail.com>
2017-05-12 09:51:52 -0400

    Refactors game times to GMT

--- app/utils/mlbHelper.js
+++ app/utils/mlbHelper.js
@@ -4,50 +4,43 @@ export default class mlbHelper {
     const time = game.event_time;
     // Parses game time from MLB game data
     if (time.indexOf(':') == 1) {
-      hours = parseInt(time.substr(0,1));
-      minutes = parseInt(time.substr(2,2));
+      hours = parseInt(time.substr(0, 1));
+      minutes = parseInt(time.substr(2, 2));
     } else {
-      hours = parseInt(time.substr(0,2));
-      minutes = parseInt(time.substr(3,2));
+      hours = parseInt(time.substr(0, 2));
+      minutes = parseInt(time.substr(3, 2));
     }
     // Converts time to 24 hr format
-    if (time.indexOf('PM') && time.substr(0,2) != 12) {
+    if (time.indexOf('PM') && time.substr(0, 2) != 12) {
       hours += 12;
     }
     // Creates date object at game time
-    const d = new Date(dateInfo['Day'+day+'Date'].year,dateInfo['Day'+day+'Date'].monthNum,dateInfo['Day'+day+'Date'].day,hours,minutes);
+    const d = new Date(dateInfo['Day' + day + 'Date'].year, dateInfo['Day' + day + 'Date'].monthNum, dateInfo['Day' + day + 'Date'].day, hours, minutes);
     // Obtains game time in ms
     let ms = parseInt(d.getTime());
     const hr = 3600000;
-    // Converts EDT game time to stadium local time
-    switch (game.home_name_abbrev) {
-      case 'CHC':
-      case 'CWS':
-      case 'MIL':
-      case 'TEX':
-      case 'HOU':
-      case 'STL':
-      case 'KC':
-      case 'MIN':
-        ms -= (hr * 1);
-        break;
-      case 'COL':
-      case 'ARI':
-        ms -= (hr * 2);
-        break;
-      case 'LAD':
-      case 'LAA':
-      case 'SD':
-      case 'OAK':
-      case 'SF':
-      case 'SEA':
-        ms -= (hr * 3);
-        break;
+    // Converts EST/EDT game time to GMT time
+    if (weather) {
+      ms -= hr * weather.offset;
+    } else {
+      // Converts time for DOME stadiums
+      ms += (hr * 5);
+      switch (game.home_name_abbrev) {
+        case 'MIL':
+        case 'HOU':
+          ms += (hr * 1);
+          break;
+        case 'ARI':
+          ms += (hr * 2);
+          break;
+        case 'SEA':
+          ms += (hr * 3);
+          break;
+      }
     }
     return {
       park: game.home_name_abbrev,
-      time: ms};
+      time: ms
+    };
   }
-  
-  
 }
