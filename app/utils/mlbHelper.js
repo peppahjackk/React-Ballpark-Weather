cd181ed550bc49f0e0f29804f9852e1b6e14068a
theodore.moke <theodore.moke@gmail.com>
2017-05-16 11:14:19 -0400

    Normalizes game/date time to always reflect accurate hourly weather data

--- app/utils/mlbHelper.js
+++ app/utils/mlbHelper.js
@@ -2,6 +2,9 @@ export default class mlbHelper {
   static convertTime(game, weather, day, dateInfo) {
     let hours, minutes;
     const time = game.event_time;
+    const tempDate = new Date();
+    var utcMonth = dateInfo['Day' + day + 'Date'].monthNum;
+    var utcDay = dateInfo['Day' + day + 'Date'].day;
     // Parses game time from MLB game data
     if (time.indexOf(':') == 1) {
       hours = parseInt(time.substr(0, 1));
@@ -14,30 +17,33 @@ export default class mlbHelper {
     if (time.indexOf('PM') && time.substr(0, 2) != 12) {
       hours += 12;
     }
+    // Converts EST/EDT game time to GMT time
+    hours += 4;
+    // Converts GMT time to system time
+    hours -= (tempDate.getTimezoneOffset() /60)
+    if (hours > 23) {
+      hours -= 24;
+      // Changes day and month data if system time is past est game day
+      if ([0,2,4,6,7,9].indexOf(utcMonth) > -1) {
+        if (day === 31) {
+          utcMonth++;
+          utcDay = 1;
+        } else {
+          utcDay++;
+        }
+      } else if ([3,5,8,10].indexOf(utcMonth) > -1) {
+        if (utcDay === 30) {
+          utcMonth++;
+          utcDay = 1;
+        } else {
+          utcDay++;
+        }
+      }
+    }
     // Creates date object at game time
-    const d = new Date(dateInfo['Day' + day + 'Date'].year, dateInfo['Day' + day + 'Date'].monthNum, dateInfo['Day' + day + 'Date'].day, hours, minutes);
+    const d = new Date(dateInfo['Day' + day + 'Date'].year, utcMonth, utcDay, hours, minutes);
     // Obtains game time in ms
     let ms = parseInt(d.getTime());
-    const hr = 3600000;
-    // Converts EST/EDT game time to GMT time
-    /* if (weather) {
-      ms -= hr * weather.offset;
-    } else {
-      // Converts time for DOME stadiums
-      ms += (hr * 4);
-      switch (game.home_name_abbrev) {
-        case 'MIL':
-        case 'HOU':
-          ms += (hr * 1);
-          break;
-        case 'ARI':
-          ms += (hr * 2);
-          break;
-        case 'SEA':
-          ms += (hr * 3);
-          break;
-      }
-    } */
     return {
       park: game.home_name_abbrev+game.game_nbr,
       time: ms
