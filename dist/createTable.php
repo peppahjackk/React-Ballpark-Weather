16d60f7c2517fd44d31b443a959b14cbc3e0d90e
theodore.moke <theodore.moke@gmail.com>
2017-05-27 10:01:31 -0400

    Adds timestamps to table building statements

--- dist/createTable.php
+++ dist/createTable.php
@@ -11,28 +11,30 @@ if ($conn->connect_error) {
 }
 
 // sql to create GameData table
-/* $sql = 'CREATE TABLE GameData
+$sql = 'CREATE TABLE GameData
 (
   id INT AUTO_INCREMENT PRIMARY KEY,
   day INT NOT NULL,
-  data VARCHAR(65500)
-)'; */
+  data VARCHAR(65500),
+  ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)';
 
 // create ActiveParks table
 /* $sql = 'CREATE TABLE ActiveParks
   (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    park VARCHAR(3)
+    park VARCHAR(3),
+    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
   )'; */
 
 // create WeatherData table
-$sql = 'CREATE TABLE WeatherData
+/* $sql = 'CREATE TABLE WeatherData
   (
     id INT AUTO_INCREMENT PRIMARY KEY,
     park VARCHAR(3),
     data VARCHAR(65000),
     ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-  )';
+  )'; */
 
 if ($conn->query($sql) === TRUE) {
   echo 'Table created successfully';
